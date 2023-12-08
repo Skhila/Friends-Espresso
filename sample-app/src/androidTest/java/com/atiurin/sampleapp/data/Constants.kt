60056508595125a0f6bfdd25b1069aa32a0cc76e
Skhila <bachanaskhiladze@gmail.com>
2023-12-09 00:56:07 +0400

    Created Tests With Parallel Testing

---
+++ sample-app/src/androidTest/java/com/atiurin/sampleapp/data/Constants.kt
@@ -0,0 +1,15 @@
+package com.atiurin.sampleapp.data
+
+object Constants {
+    const val friendName = "Gunther"
+    const val friendGreeting = "Hello"
+
+    const val friendNameEmmet = "Emmet Brown"
+    const val emmetMessage = "let's go for a drink"
+
+    const val friendNameFriend17 = "Friend17"
+    const val friend17Message = "please change your name"
+
+    const val customClicksItemName = "Custom Clicks"
+    const val uiElementsMenuItemName = "UiElements"
+}
